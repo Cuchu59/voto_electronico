667c454a287e58f7aa0f7629a3a94f5ab90852a2
cuchud <cuchu.sere@gmail.com>
2026-03-19 11:53:11 -0300

    Add MQTT client implementation and unit tests
    - Implemented PubSubClient class for MQTT communication in PubSubClient.cpp and PubSubClient.h.
    - Added unit tests for basic MQTT functionality in mqtt_basic.py and mqtt_publish_in_callback.py.
    - Created settings.py for configuration of server IP addresses.
    - Developed testsuite.py to manage test execution and environment setup.
    - Established directory structure for tests and examples.

--- voto_electronico_esp32/src/main.cpp
+++ voto_electronico_esp32/src/main.cpp
@@ -1,19 +1,83 @@
 #include <Arduino.h>
+/*
+   ESP8266 o ESP32 (ver las modificaciones)
+   Este script sube al server un valor "aleatorio" en el topic "PCOLUNGA_EL_MEJOR_PROFE_DE_ANAYDIS" 
 
-// put function declarations here:
-int myFunction(int, int);
-int myFunction(int x,int y) {
-  return x+y;
-}
+*/
+
+#include <WiFi.h> // Para el ESP32
+WiFiClient WIFI_CLIENT;
+
+#include <PubSubClient.h>
+PubSubClient MQTT_CLIENT;
+
+// Nombre y contraseña de tu red WiFi.
+const char* ssid = "UA-Alumnos";
+const char* password = "41umn05WLC";
+
+// Def of functions
+void reconnect();
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
+  
+  delay(10);
+
+  Serial.println();
+  Serial.print("Conectando con ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi conectado.");
+  Serial.println("IP: ");
+  Serial.print(WiFi.localIP());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Running test");
-  delay(5000);  // 5 seconds delay 
+  // Continuamente comprueba si tiene conexión con MQTT,
+  // en caso de que no tenga, reconecta.
+  if (!MQTT_CLIENT.connected()) {
+    reconnect();
+  }
+
+  // Publicar un mensaje. Publish.
+  // Convierte el entero a char. DEBE ser char.
+  int aleatorio = random(1,90);
+  Serial.println(aleatorio);
+  String aleatorioString = String(aleatorio);
+  char alea[6];
+  aleatorioString.toCharArray(alea, 6);
+
+  //                   Topic / valor
+  MQTT_CLIENT.publish("PCOLUNGA_EL_MEJOR_PROFE_DE_ANAYDIS/aleatorio", alea);
+
+  // Espera antes de Publicar otro aleatorio.
+  delay(5000);
+}
+
+// Reconecta con MQTT broker
+void reconnect() {
+  // MQTT_CLIENT.setServer("192.168.1.206", 1883); // si uso un servidor local <ver IP correcta>
+    MQTT_CLIENT.setServer("broker.hivemq.com", 1883);  // servidor gratuito
+
+  MQTT_CLIENT.setClient(WIFI_CLIENT);
+
+  // Intentando conectar con el broker.
+  while (!MQTT_CLIENT.connected()) {
+    Serial.println("Intentando conectar con MQTT.");
+    MQTT_CLIENT.connect("PCOLUNGA_EL_MEJOR_PROFE_DE_ANAYDIS"); // Escribe cualquier nombre.
+
+    // Espera antes de volver a intentarlo.
+    delay(3000);
+  }
+
+  Serial.println("Conectado a MQTT.");
 }
 
